a7d31c9240de0f015e37142e1a864c0f530511a9
Jean A. Miranda Vargas <132174558+zarkydarky@users.noreply.github.com>
2024-07-11 21:32:32 -0400

    Update and rename 4_Minutes_In_A_Year.py to 04_Minutes_In_A_Year.py

---
+++ Part 1/04_Minutes_In_A_Year.py
@@ -0,0 +1,2 @@
+print("Minutes in a year: ")
+print(365*24*60) # 365 days, 24 hours, 60 minutes in an hour
--- Part 1/4_Minutes_In_A_Year.py
+++
@@ -1,2 +0,0 @@
-print("Minutes in a year: ")
-print(365*24*60) # 365 days, 24 hours, 60 minutes in an hour
