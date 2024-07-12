c89d265dc6d686130e5d22625b6ff11becd22e73
Jean A. Miranda Vargas <132174558+zarkydarky@users.noreply.github.com>
2024-07-11 21:23:50 -0400

    Add files via upload

---
+++ Part 1/8_Name_And_Address.py
@@ -0,0 +1,7 @@
+name = input("Given name: ")
+family_name = input("Family name: ")
+street_address = input("Street address: ")
+city_and_postal_code = input("City and postal code: ")
+print(name + " " + family_name)
+print(street_address)
+print(city_and_postal_code)
